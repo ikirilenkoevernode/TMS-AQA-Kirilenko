0261dbb0c48104c71224ef65f4d640e755d8127a
Iaroslav Kirilenko <kirilenkoiaroslav@gmail.com>
2026-08-01 23:02:52 +0200

    Задание 3. «Поиск в хаосе»

--- MiniAutomationToolkit/MiniAutomationToolkit.App/Program.cs
+++ MiniAutomationToolkit/MiniAutomationToolkit.App/Program.cs
@@ -1,7 +1,10 @@
 ﻿using MiniAutomationToolkit.Core;
 using MiniAutomationToolkit.Core.Models;
+using MiniAutomationToolkit.Core.Helpers;
+// 1 задание
 var test = new PrintReference();
 Console.WriteLine(test.Hello());
+// 2 задание
 Console.WriteLine(test.Discount(ClientType.Vip, 500));
 Console.WriteLine(test.Discount(ClientType.Vip, 2000));
 Console.WriteLine(test.Discount(ClientType.Premium, 800));
@@ -10,3 +13,45 @@ Console.WriteLine(test.Discount(ClientType.Premium, 1500));
 Console.WriteLine(test.Discount(ClientType.Regular, 500));
 Console.WriteLine(test.Discount(ClientType.Regular, 1500));
 Console.WriteLine(test.Discount(ClientType.Regular, 1000));
+// 3 задание
+var fileNames = new List<string>
+{
+    "debug_742.txt",
+    "screen_019.png",
+    "error_2242.log",
+    "trace_007.txt",
+    "screen_003.png",
+    "warning_842.log",
+    "debug_015.txt",
+    "screen_021.png",
+    "crash_422.log",
+    "report_009.txt",
+    "screen_014.png",
+    "error_731.log",
+    "dump_242.txt",
+    "screen_006.png",
+    "debug_328.log",
+    "trace_011.txt",
+    "screen_018.png",
+    "fatal_542.log",
+    "screen_025.png",
+    "session_902.txt"
+};
+var fileNamesWithoutScreenshots = new List<string>
+{
+    "debug_742.txt",
+    "error_2242.log",
+    "trace_007.txt",
+    "warning_842.log",
+    "debug_015.txt",
+    "crash_422.log",
+    "report_009.txt",
+    "error_731.log",
+    "dump_242.txt",
+    "debug_328.log",
+    "trace_011.txt",
+    "fatal_542.log",
+    "session_902.txt"
+};
+Console.WriteLine(FileSearcher.FindFirstScreenshot(fileNames));
+Console.WriteLine(FileSearcher.FindFirstScreenshot(fileNamesWithoutScreenshots));
---
+++ MiniAutomationToolkit/MiniAutomationToolkit.Core/Helpers/FileSearcher.cs
@@ -0,0 +1,20 @@
+﻿namespace MiniAutomationToolkit.Core.Helpers
+{
+    public static class FileSearcher
+    {
+        public static string FindFirstScreenshot(List<string> fileNames)
+        {
+            var pngFiles = fileNames.Where(file => file.EndsWith(".png", StringComparison.OrdinalIgnoreCase));
+
+            if (pngFiles.Any())
+            {
+                return pngFiles.FirstOrDefault()!;
+            }
+            else
+            {
+                throw new FileNotFoundException("No screenshots found in the provided list.");
+            }
+        }
+    }
+
+}
